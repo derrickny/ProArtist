7fb739f19526f1feed17b1836ea79ff01968fbff
nyagaderrick <nyagad@icloud.com>
2024-04-19 23:15:21 +0300

    setting up and working on the frontend

---
+++ frontend/src/components/SideNavbar.tsx
@@ -0,0 +1,11 @@
+import exp from 'constants';
+import React from 'react';
+
+
+type props = { }
+
+export default function SideNavbar({}: props) {
+    return (
+        <div>Sidebar</div>
+    )
+    }   
