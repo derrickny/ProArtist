26e859adad6e7c1974541dad8c0355ef0b84ff61
nyagaderrick <nyagad@icloud.com>
2024-05-28 23:26:59 +0300

    switching to neon database

--- backend/proartist_backend/settings.py
+++ backend/proartist_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+# Add these at the top of your settings.py
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,15 +92,21 @@ WSGI_APPLICATION = 'proartist_backend.wsgi.application'
 #     }
 # }
 
+
+
+# Replace the DATABASES section of your settings.py with this
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'posgres',
-        'USER': 'postgres.fhxvunehsrrwacyagebh',
-        'PASSWORD': '6897Ed29n@ESqVM',
-        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
-        'PORT': '5432',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv('PGDATABASE'),
+    'USER': getenv('PGUSER'),
+    'PASSWORD': getenv('PGPASSWORD'),
+    'HOST': getenv('PGHOST'),
+    'PORT': getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
 }
 
 # Password validation
