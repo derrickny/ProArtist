1f75f924268d65e198f0e5e005929d0e2a38a7a5
nanjala7 <93724421+nanjala7@users.noreply.github.com>
2024-05-31 21:59:14 +0300

    creating  appointments

--- frontend/src/app/appointments/page.tsx
+++ frontend/src/app/appointments/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import appointmentevent from '@/components/appointmentevent';
 
 type props = { }
 
 export default function Appointments({}: props) {
     return (
         <div>
-            Appointments
+           // Appointments
+           
         </div>
     )
 }
+
+
